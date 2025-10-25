153bf8697fa8acb152248bfaa09c8cb512a0c4ab
frxd-sloureiro <soprazuarhuehue@gmail.com>
2025-10-24 23:16:44 -0300

    Começo da implementação da web view e mudança de empacotamento para war #1

--- SGCO/SGCO-common/src/main/java/sgco/sgco/common/SGCOCommon.java
+++ SGCO/SGCO-common/src/main/java/sgco/sgco/common/SGCOCommon.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package sgco.sgco.common;
-
-/**
- *
- * @author pedro
- */
 public class SGCOCommon {
 
     public static void main(String[] args) {
