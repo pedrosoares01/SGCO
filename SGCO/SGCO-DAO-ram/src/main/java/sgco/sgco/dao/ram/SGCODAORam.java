45e89eb5707849c8453478ce29f6056cea9547e4
Davi Ramos <rdavi6558@gmail.com>
2025-11-11 19:29:29 -0300

    Correção nos arquivos para não da conflito na main. #8

---
+++ SGCO/SGCO-DAO-ram/src/main/java/sgco/sgco/dao/ram/SGCODAORam.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sgco.sgco.dao.ram;
+
+/**
+ *
+ * @author pedro
+ */
+public class SGCODAORam {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SGCO/SGCO-common/src/main/java/sgco/sgco/common/SGCOCommon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sgco.sgco.common;
+
+/**
+ *
+ * @author pedro
+ */
+public class SGCOCommon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
